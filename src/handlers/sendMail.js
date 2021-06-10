df191f3a997328287f1337d77884cb8288da66e3
Yousuf Qutubuddin <usuf52@gmail.com>
2021-06-10 18:26:08 +0500

    init

---
+++ src/handlers/sendMail.js
@@ -0,0 +1,35 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
+async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log("record ==> ", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
+  const params = {
+    Source: "zain.khaan147@gmail.com",
+    Destination: { ToAddresses: [recipient] },
+    Message: {
+      Body: {
+        Text: {
+          Data: body,
+        },
+      },
+      Subject: {
+        Data: subject,
+      },
+    },
+  };
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const handler = sendMail;
